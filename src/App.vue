b05704bfbf682d09e3aedab8866eb95f62979f7d
nsinghproexe <119045598+nsinghproexe@users.noreply.github.com>
2024-08-02 17:59:57 +0530

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,127 @@
+<script setup>
+// import ConfettiCelebrationVue from './components/ConfettiCelebration.vue'
+import ConfettiExplosion from 'vue-confetti-explosion'
+import { ref, onMounted } from 'vue'
+import axios from 'axios'
+
+const BASE_URL = import.meta.env.BASE_URL || 'https://www.test.com/'
+
+// Define the spotlight state
+const spotlight = ref('')
+
+// Function to fetch data from the API
+async function fetchData() {
+  try {
+    const response = await axios.get(`${BASE_URL}spotlight/employee`)
+    spotlight.value = response.data.data
+  } catch (error) {
+    console.error('Failed to fetch spotlight employee:', error)
+    spotlight.value = 'Sorry we are facing some issues. Please try again later...'
+  }
+}
+
+// Call fetchData when the component mounts
+onMounted(fetchData)
+</script>
+
+<template>
+  <div class="main">
+    <div class="confetti">
+      <ConfettiExplosion :particleSize="10" />
+    </div>
+
+    <div class="main-content">
+      <div class="section">
+        <div>
+          <!-- <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" /> -->
+          <img src="https://www.proexelancers.com/images/proexelancers-logo.svg" />
+
+          <div class="wrapper">
+            <!-- <HelloWorld msg="You did it!" /> -->
+          </div>
+        </div>
+
+        <div>
+          <div class="spotlight">
+            Current person on spotlight - {&#123;
+            <span class="employee-name">{{ spotlight }}</span> &#125;}
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+.timer {
+  display: flex;
+  justify-content: center;
+}
+
+.confetti {
+  display: flex;
+  justify-content: center;
+}
+
+.main {
+  overflow: hidden;
+}
+
+.main-content {
+  height: 93vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.employee-name {
+  background: linear-gradient(
+    169deg,
+    rgb(24 10 255) 0%,
+    rgba(13, 3, 68, 1) 9%,
+    rgb(253 139 18) 33%,
+    rgba(249, 171, 0, 1) 97%,
+    rgb(0 186 223) 100%
+  );
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  .spotlight {
+    font-size: 50px;
+  }
+}
+
+@media (min-width: 480px) {
+  .spotlight {
+    color: white;
+    font-size: 30px;
+  }
+}
+</style>
